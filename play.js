a8763f9a28bc6680708a43a50faeea6c57dedf01
Songklod <itoon.sit@gmail.com>
2022-07-26 13:17:59 +0700

    lesson1: let vs const and show weakly type language

---
+++ play.js
@@ -0,0 +1,19 @@
+// let and const
+
+const name = "Songklod";
+const age = "30";
+let classRoom = "CodeTreveler";
+// classRoom = classRoom + " " + "X DataRockie";
+classRoom = `${classRoom} X DataRockie`;
+
+console.log(name);
+console.log(age);
+console.log(`===========`);
+
+// weakly type language
+console.log(`classroom: ${classRoom}`);
+console.log(`typeof classroom: ${typeof classRoom}`);
+
+classRoom = 4;
+console.log(`classroom: ${classRoom}`);
+console.log(`typeof classroom: ${typeof classRoom}`);
