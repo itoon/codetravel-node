b662b4fa59e33c1e800651a3a9d66d644e944e40
Songklod <itoon.sit@gmail.com>
2022-07-26 15:21:27 +0700

    lesson4: play with loop items

--- play.js
+++ play.js
@@ -13,5 +13,20 @@ const person = {
   },
 };
 
-console.log(person.greet());
-console.log(person.greet2());
+// console.log(person.greet());
+// console.log(person.greet2());
+
+const books = ["Start with why", "Coding is matter", "Coding"];
+for (const book of books) {
+  console.log(book);
+}
+console.log("========");
+
+for (let i = 0; i < books.length; i++) {
+  console.log(`${i}: ${books[i]}`);
+}
+console.log("========");
+
+books.forEach((book, index) => {
+  console.log(`${index}: ${book}`);
+});
