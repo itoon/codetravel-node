ecf827aca58a1f4e3867fc3ed222e39689b2c262
Songklod <itoon.sit@gmail.com>
2022-07-26 15:11:12 +0700

    lesson3: object and function

--- play.js
+++ play.js
@@ -1,43 +1,17 @@
-// let and const
+const person = {
+  name: "Songklod",
+  age: 30,
 
-const name = "Songklod";
-const age = "30";
-let classRoom = "CodeTreveler";
-// classRoom = classRoom + " " + "X DataRockie";
-classRoom = `${classRoom} X DataRockie`;
+  // object this
+  greet() {
+    return `Hello: ${this.name}`;
+  },
 
-//function
-function joinClassRoom(name, classRoom) {
-  return "function: " + name + " join " + classRoom;
-}
-
-// ansynimous function
-const ansynimousJoinClassRoom = function (name, classRoom) {
-  return "ansynimous function: " + name + " join " + classRoom;
+  // global this
+  greet2: () => {
+    return `Hello: ${this.name}`;
+  },
 };
 
-//arrow function
-const arrowJoinClassRoom = (name, classRoom) => {
-  // this
-  //console.log(this);
-  return "ansynimous function: " + name + " join " + classRoom;
-};
-
-console.log(joinClassRoom(name, classRoom));
-
-// play with ansynimous function
-console.log(ansynimousJoinClassRoom(name, classRoom));
-
-//play with arrow function
-console.log(arrowJoinClassRoom(name, classRoom));
-
-const add = (num1, num2) => {
-  return num1 + num2;
-};
-
-const minus = (num1, num2) => {
-  return num1 - num2;
-};
-
-console.log(add(7, 10));
-console.log(minus(7, 10));
+console.log(person.greet());
+console.log(person.greet2());
