39ef704bdd46fe94e94aecb04f043a23320984b4
Songklod <itoon.sit@gmail.com>
2022-07-29 19:33:01 +0700

    lesson call back function

--- play.js
+++ play.js
@@ -2,10 +2,20 @@ console.log("Start");
 
 setTimeout(() => {
   console.log("Timer 3 sec Done!!");
-}, 2000);
+}, 3000);
 
 setTimeout(() => {
   console.log("Timer 0 sec Done!!");
 }, 0);
 
 console.log("Stop");
+
+const fetchData = (callback) => {
+  setTimeout(() => {
+    callback("Timer 2 sec Done!!");
+  }, 2000);
+};
+
+fetchData((text) => {
+  console.log(text);
+});
